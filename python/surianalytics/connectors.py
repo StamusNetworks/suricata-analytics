e9e9ceca22ec5704835b0d24842e3bda10aa1d8c
Nicolas Frisoni <nfrisoni@stamus-networks.com>
2024-06-03 17:28:09 +0200

    python: helper method to generate latex correctly
    Ref: scirius#7867 #36

--- python/surianalytics/connectors.py
+++ python/surianalytics/connectors.py
@@ -30,6 +30,7 @@ from copy import deepcopy
 import networkx as nx
 import pandas as pd
 import subprocess
+from IPython.display import display, Markdown
 
 from dotenv import dotenv_values
 from datetime import datetime, timedelta, timezone
@@ -506,6 +507,32 @@ def check_str_bool(val: str) -> bool:
         return False
     else:
         raise ValueError("invalid truth value {}".format(val))
+
+
+class Report:
+    COUNTER = 0
+
+    def __init__(self, debug=False) -> None:
+        self.debug = debug
+
+    def output(self, df: pd.DataFrame, column_format=None, size=None):
+        if size is not None:
+            df = df[:size]
+
+        if self.debug:
+            return display(df)
+
+        output = f'{Report.COUNTER}.tex'
+        Report.COUNTER += 1
+
+        if df is not None and len(df):
+            df.to_latex(output, column_format=column_format, index=False, escape=True)
+        else:
+            if os.path.exists(output):
+                os.remove(output)
+            open(output, 'a').close()
+
+        display(Markdown(f'STAMUSINPUT: {output}'))
 
 
 def getGitRoot():
